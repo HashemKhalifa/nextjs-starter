056055a0d39e1082f893e6a6a115c7b1ec9071c1
HashemKhalifa <hashem.khalifa@gmail.com>
2022-03-12 16:04:55 +0100

    init nextjs starter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
-import '../styles/globals.css'
+import { globalStyles } from '../shared/styles'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>  {globalStyles}
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
